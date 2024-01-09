d92ebfc396948e432117231aa18d78f1bd5aa71d
TaariqM <taariqm9@my.yorku.ca>
2024-01-09 16:50:50 -0500

    Added spotify albums API endpoint

--- sp-playlists-frontend/src/pages/Playlists.tsx
+++ sp-playlists-frontend/src/pages/Playlists.tsx
@@ -7,6 +7,10 @@ const Playlists = () => {
   const { codeParam } = useParams();
   const [accessToken, setAccessToken] = useState("");
   const [userSavedSongs, setUserSavedSongs] = useState({});
+  const [songInfo, setSongInfo] = useState({
+    track: [],
+    genres: [],
+  });
   const redirectURI: string = "http://localhost:5173/";
   const clientID = import.meta.env.VITE_REACT_APP_CLIENT_ID;
   const clientSecret = import.meta.env.VITE_REACT_APP_CLIENT_SECRET;
@@ -15,10 +19,10 @@ const Playlists = () => {
 
   const handleOnClick = async (e: any) => {
     e.preventDefault();
-
+    let savedSongs;
     // console.log("Test");
     try {
-      const savedSongs = await axios.get(
+      savedSongs = await axios.get(
         "https://api.spotify.com/v1/me/tracks?limit=50",
         {
           headers: {
@@ -28,12 +32,32 @@ const Playlists = () => {
         }
       );
       console.log(savedSongs.data.items);
-      // setUserSavedSongs(savedSongs);
+      setUserSavedSongs(savedSongs.data.items);
     } catch (error) {
       console.log("Error fetching saved songs: ", error);
     }
 
-    // console.log(userSavedSongs);
+    // getAllGenres(userSavedSongs);
+    getAllGenres(savedSongs?.data.items);
+  };
+
+  const getAllGenres = async (songs: any) => {
+    try {
+      for (let i = 0; i < songs.length; i++) {
+        const album = axios.get(
+          `https://api.spotify.com/v1/albums/${songs[i].track.album.id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        console.log(album);
+      }
+    } catch (error) {
+      console.log("Error fetching alblums: ", error);
+    }
   };
 
   useEffect(() => {
@@ -69,16 +93,18 @@ const Playlists = () => {
     getAccessToken();
   }, []);
 
-  console.log(accessToken);
+  // console.log(accessToken);
 
   return (
     <div className="playlists">
-      <div className="empty-playlists-container">
-        <h1>Please Click On A Button To Generate A Playlist</h1>
-      </div>
+      <div className="container">
+        <div className="empty-playlists-container">
+          <h1>Please Click On A Button To Generate A Playlist</h1>
+        </div>
 
-      <div className="generator-btn">
-        <button onClick={handleOnClick}>Generate Playlists</button>
+        <div className="generator-btn">
+          <button onClick={handleOnClick}>Generate Playlists</button>
+        </div>
       </div>
     </div>
   );
